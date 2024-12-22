c33b57510f76e511f77dfbafbeb6ad47985b10a5
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-12-22 19:05:57 +0600

    error page modified

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Error = () => {
   return (
-    <div>
-      <h2>error</h2>
+    <div className="min-h-screen flex flex-col justify-center items-center font-inter">
+      <h2 className="text-7xl font-montserrat font-bold mb-2">404</h2>
+      <p className="text-2xl">Sorry, we were unable to find that page</p>
+      <p className="mt-4">
+        Start from{" "}
+        <Link to="/" className="underline font-medium hover:font-semibold">
+          home page
+        </Link>
+      </p>
     </div>
   );
 };
