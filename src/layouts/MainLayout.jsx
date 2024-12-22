dae85f6c6ffba89de1412e634f553d52ce93ec8a
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-12-22 13:09:55 +0600

    added raw navbar from daisy

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
+      <nav className="bg-default text-white font-inter">
+        <Navbar></Navbar>
+      </nav>
       <Outlet></Outlet>
     </div>
   );
