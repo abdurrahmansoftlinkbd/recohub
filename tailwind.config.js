d66258b45279c8472468639b535b79926533d8f7
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-12-22 12:55:05 +0600

    added custom colors and fontFamily in tailwindConfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        default: "#007BFF",
+        light: "#339CFF",
+        dark: "#0056B3",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "serif"],
+        inter: ["Inter", "serif"],
+      },
+    },
   },
   plugins: [daisyui],
 };
