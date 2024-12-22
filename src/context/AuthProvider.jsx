5457d98f96abf0792ae6f39e9663fbfeca4a2801
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-12-22 15:26:30 +0600

    added react prop types

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,7 +1,12 @@
 import AuthContext from "./AuthContext";
+import PropTypes from "prop-types";
 
 const AuthProvider = ({ children }) => {
   return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.object,
 };
 
 export default AuthProvider;
