5ae317e1bf8d95a9a334446ce6771d217834881b
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-12-22 14:45:03 +0600

    added authProvider

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,7 @@
+import AuthContext from "./AuthContext";
+
+const AuthProvider = ({ children }) => {
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
