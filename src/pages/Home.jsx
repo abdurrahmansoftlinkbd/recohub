7a9675dbd5790ee447a2483771fe1998dd73ac10
abdurrahmansoftlinkbd <kids.hasib.softlinkbd@gmail.com>
2024-12-23 09:25:03 +0600

    added accordion

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
+import Accordion from "../components/Accordion";
 import Slider from "../components/Slider";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Slider></Slider>
-    </div>
+      <section className="container w-11/12 mx-auto font-inter">
+        <Accordion></Accordion>
+      </section>
+    </>
   );
 };
 
